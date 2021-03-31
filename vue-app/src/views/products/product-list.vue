e7591dce3d144ec63ad53edc1ea6d10e31bacf7a
VMhimanshu <80322384+VMhimanshu@users.noreply.github.com>
2021-03-31 20:33:59 +0530

    Update product-list.vue

--- vue-app/src/views/products/product-list.vue
+++ vue-app/src/views/products/product-list.vue
@@ -31,7 +31,7 @@ export default {
 
 <template>
   <div>
-    <div v-if="!products.length">Loading data ...</div>
+    <div v-if="!products.length">It seems you have nothing stored yet... Let's try add some task !</div>
     <ul class="list">
       <li
         v-for="(product, index) in products"
